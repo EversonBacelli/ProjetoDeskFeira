702fd0cd54613dc9932d6e8a5c22269a87e3a6f8
Helio <Helio@AKI-PC>
2019-05-23 17:16:01 -0300

    alteração da venda concluida

--- src/Model/Venda.java
+++ src/Model/Venda.java
@@ -4,11 +4,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Venda {
-	List<ProdutoEstoque> item = new ArrayList();
-	String data;
-	int qtdVenda;
+
+	int id;
+	// lista de lote de produto
+	List<LoteProduto> item = new ArrayList();
+	// data da venda
+	String dataVenda;
+	// Valor total da venda
 	double valorTotal;
-	int nVenda;
+	// variavel que guarda a quantidade para diminuir dentro do lote
+	int qtdVenda;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public List<LoteProduto> getItem() {
+		return item;
+	}
+	public void setItem(List<LoteProduto> item) {
+		this.item = item;
+	}
+	public String getDataVenda() {
+		return dataVenda;
+	}
+	public void setDataVenda(String dataVenda) {
+		this.dataVenda = dataVenda;
+	}
+	public double getValorTotal() {
+		return valorTotal;
+	}
+	public void setValorTotal(double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	public int getQtdVenda() {
+		return qtdVenda;
+	}
+	public void setQtdVenda(int qtdVenda) {
+		this.qtdVenda = qtdVenda;
+	}
 	
 	
 }
