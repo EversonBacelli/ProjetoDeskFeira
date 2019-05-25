4233e3bd479ca70a4fb2313f03baaeaf3602f8d4
Helio <Helio@AKI-PC>
2019-05-25 11:38:05 -0300

    atualização do:
    Controle Venda 
    &
    Venda

--- src/Control/ControleVenda.java
+++ src/Control/ControleVenda.java
@@ -1,10 +1,45 @@
 package Control;
 
-public class ControleVenda 
-{
-	
-	void realizarVenda() {
-		
-	}
+import java.util.ArrayList;
+import java.util.List;
 
+import Model.LoteProduto;
+import Model.Venda;
+
+public class ControleVenda {
+	List<LoteProduto> listaLoteProdutos = new ArrayList<>();
+	List<Venda> lista_venda = new ArrayList<>();
+	
+	public void realizarVenda(Venda venda) {
+
+		// criando o metodo para usar a alteração da quantidade dentro do
+		// estoque
+		ControleDeLoteProduto controleProduto = new ControleDeLoteProduto();
+		List<LoteProduto> listaLoteProdutos = venda.getListaLoteProduto();
+		double valorTotal = 0;
+		int qtdTotalProdutos = 0;
+
+		// diminuindo o valor dos LoteProdutos comprados
+		for (LoteProduto loteProduto : listaLoteProdutos) {
+			// 0 = administrador
+			// 1 = vendedor
+			// 2 = estoquista
+			controleProduto.alterarEstoque(loteProduto, 1);
+			// como vou saber se o produto foi alterado ou nao?
+			// Se eu não saber os itens que foram alterado, como vou fazer para
+			// garantir que, o produto que eu estou cobrando em R$ esta sendo
+			// diminuido no lote?
+
+			// se estoque foi alterado, entao vamos somar o valor da compra
+			valorTotal += loteProduto.getproduto.getValor() * loteProduto.getQuantidade();
+
+			// se estoque foi alterado, gravar a quantidade total de um produto
+			qtdTotalProdutos += loteProduto.getQuantidade();
+		}
+		venda.setValorTotal(valorTotal);
+		venda.setQtdVenda(qtdTotalProdutos);
+		
+		//salvando a venda efetuada
+		lista_venda.add(venda);
+	}
 }
--- src/Model/Venda.java
+++ src/Model/Venda.java
@@ -6,45 +6,49 @@ import java.util.List;
 public class Venda {
 
 	int id;
-	// lista de lote de produto
-	List<LoteProduto> item = new ArrayList();
-	// data da venda
+	List<LoteProduto> listaLoteProduto;
 	String dataVenda;
-	// Valor total da venda
 	double valorTotal;
-	// variavel que guarda a quantidade para diminuir dentro do lote
 	int qtdVenda;
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	public List<LoteProduto> getItem() {
-		return item;
+
+	public List<LoteProduto> getListaLoteProduto() {
+		return listaLoteProduto;
 	}
-	public void setItem(List<LoteProduto> item) {
-		this.item = item;
+
+	public void setListaLoteProduto(List<LoteProduto> listaLoteProduto) {
+		this.listaLoteProduto = listaLoteProduto;
 	}
+
 	public String getDataVenda() {
 		return dataVenda;
 	}
+
 	public void setDataVenda(String dataVenda) {
 		this.dataVenda = dataVenda;
 	}
+
 	public double getValorTotal() {
 		return valorTotal;
 	}
+
 	public void setValorTotal(double valorTotal) {
 		this.valorTotal = valorTotal;
 	}
+
 	public int getQtdVenda() {
 		return qtdVenda;
 	}
+
 	public void setQtdVenda(int qtdVenda) {
 		this.qtdVenda = qtdVenda;
 	}
-	
-	
+
 }
