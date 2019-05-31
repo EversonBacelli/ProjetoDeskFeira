67faabdd683286be60a257fb45c587fb5a2a0fec
Helio <Helio@AKI-PC>
2019-05-31 18:19:32 -0300

    tela login atualizado

--- src/View/TelaLogin.java
+++ src/View/TelaLogin.java
@@ -18,13 +18,10 @@ public class TelaLogin extends Application implements EventHandler<ActionEvent> 
 
 	static Stage stageAux;
 	GridPane pane;
-
 	Label lblLogin;
 	Label lblSenha;
-
 	TextField txtLogin;
 	TextField txtSenha;
-
 	Button btnEntrar;
 
 	public static void main(String[] args) {
@@ -32,39 +29,35 @@ public class TelaLogin extends Application implements EventHandler<ActionEvent> 
 	}
 
 	@Override
-	public void start(Stage telaCadastro) throws Exception {
-		stageAux = telaCadastro;
+	public void start(Stage stage) throws Exception {
+		stageAux = stage;
+		
 		// Elementos de Estrutura
 		pane = new GridPane();
 
 		// Elementos da tela
 		lblLogin = new Label(" LOGIN ");
 		lblSenha = new Label(" SENHA ");
-
 		txtLogin = new TextField("");
 		txtSenha = new TextField("");
-
 		btnEntrar = new Button(" ENTRAR ");
 
 		// -----------------------------------------------------------
-
 		// Inserindo elementos
 		pane.add(lblLogin, 0, 0);
 		pane.add(lblSenha, 0, 1);
-
 		pane.add(txtLogin, 1, 0);
 		pane.add(txtSenha, 1, 1);
-
 		pane.add(btnEntrar, 1, 2);
-		// -------------------------------------
-		marginPaine();
-		adicionandoEstiloElementos();
+		// -----------------------------------------------------------
+		adicionarMargem();
+		adicionarEstilo();
 
 		pane.addEventFilter(ActionEvent.ACTION, this);
 		Scene scn = new Scene(pane, 1000, 563);
-		telaCadastro.setTitle(" TELA DE CADASTRO DE FUNCIONARIO  ");
-		telaCadastro.setScene(scn);
-		telaCadastro.show();
+		stage.setTitle(" TELA DE CADASTRO DE FUNCIONARIO  ");
+		stage.setScene(scn);
+		stage.show();
 	}
 
 	@Override
@@ -88,7 +81,7 @@ public class TelaLogin extends Application implements EventHandler<ActionEvent> 
 					e.printStackTrace();
 				}
 			} else {
-				JOptionPane.showMessageDialog(null, "Senha ou Login Invalido!!!!!!!!!");
+				JOptionPane.showMessageDialog(null, "Senha ou Login Inválido!!!!!!!!!");
 			}
 		}
 	}
@@ -100,7 +93,7 @@ public class TelaLogin extends Application implements EventHandler<ActionEvent> 
 
 
 	// Estilos dos Objetos ------------------------------------------
-	private void marginPaine() {
+	private void adicionarMargem() {
 		Insets marginTop = new Insets(50, 30, 0, 0);
 		Insets margin1 = new Insets(50, 30, 0, 60);
 		pane.setMargin(lblLogin, marginTop);
@@ -110,15 +103,11 @@ public class TelaLogin extends Application implements EventHandler<ActionEvent> 
 		pane.setMargin(btnEntrar, marginTop);
 	}
 
-	public void adicionandoEstiloElementos() {
-
+	public void adicionarEstilo() {
 		lblLogin.setFont(new Font(15));
 		lblSenha.setFont(new Font(15));
-		btnEntrar.setFont(new Font(20));
-
 		lblLogin.setStyle("-fx-font-weight: bold");
 		lblSenha.setStyle("-fx-font-weight: bold");
-		btnEntrar.setStyle("-fx-font-weight: bold");
 	}
 
 }
