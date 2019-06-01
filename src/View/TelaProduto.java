a38caff19f376bca412258ac2352607666c042d3
BRUNOFERREIRACARDOSO <BRUNOFERREIRACARDOSO@BRUNODESKTOP>
2019-06-01 04:01:01 -0300

    mudança na tela de produto, adicionando search

--- src/View/TelaProduto.java
+++ src/View/TelaProduto.java
@@ -4,6 +4,8 @@ package View;
 
 import javax.swing.JOptionPane;
 
+import com.sun.javafx.scene.control.skin.ButtonSkin;
+
 import Control.ControleProduto;
 import Model.Produto;
 import javafx.application.Application;
@@ -29,7 +31,12 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Line;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -38,19 +45,31 @@ public class TelaProduto extends Application implements EventHandler<ActionEvent
 	private TableView<Produto> table = new TableView<>();
 	private ImageView img = new ImageView(new Image("file:Images/gerenciamento_prod.png"));		
 	private BorderPane painelPrincipal = new BorderPane();
-	private GridPane painelLbl = new GridPane();
-	private GridPane painelLbl2 = new GridPane();
-	private GridPane painelManipulavel = new GridPane();
-	private FlowPane painelBtn = new FlowPane();
-	private Scene scn = new Scene(painelPrincipal, 1600, 900);
 	
-	private Label lblId = new Label("ID : ");
-	private Label lblNome = new Label("Nome : ");
-	private Label lblDescricao = new Label("Descricao : ");
-	private Label lblMax = new Label("Quantidade Maxima");
-	private Label lblMin = new Label("Quantidade Minima");
+	private VBox painelTopo = new VBox();
+	private VBox painelLateral = new VBox();
+	private VBox painelCentral = new VBox();
+	private FlowPane painelLateral1 = new FlowPane();
+	private FlowPane painelLateral2 = new FlowPane();
+	private FlowPane painelLateral3 = new FlowPane();
+	private FlowPane painelLateral4 = new FlowPane();
+	private FlowPane painelLateral5 = new FlowPane();
+	private FlowPane painelLateral6 = new FlowPane();
+	private FlowPane painelLateral7 = new FlowPane();
+	private FlowPane painelButtons = new FlowPane();
+	private FlowPane painelPesquisarElementos = new FlowPane();
+	
+	
+	private Scene scn = new Scene(painelPrincipal, 1000, 563);
+	
+	private Label lblId = new Label("ID");
+	private Label lblNome = new Label("Nome");
+	private Label lblDescricao = new Label("Descricao");
+	private Label lblMax = new Label("Qtd Maxima");
+	private Label lblMin = new Label("Qtd Minima");
 	private Label lblTempoVida = new Label("Tempo de Vida");
 	private Label lblPreco = new Label("Preco");	
+
 	
 	private TextField tfId = new TextField();
 	private TextField tfNome = new TextField();
@@ -59,13 +78,21 @@ public class TelaProduto extends Application implements EventHandler<ActionEvent
 	private TextField tfMin = new TextField();
 	private TextField tfTempoVida = new TextField();
 	private TextField tfPreco = new TextField();	
+	private TextField tfPesquisar = new TextField();
 	
 	private Button btnPesquisar = new Button("PESQUISAR");
 	private Button btnSalvar = new Button("SALVAR");
 	private Button btnExcluir = new Button("EXCLUIR");
 	
+	private Line linha = new Line();
+	
 	ControleProduto cp = new ControleProduto();
 
+	
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+	
 	@Override
 	public void start(Stage stage) throws Exception {
 		
@@ -81,16 +108,27 @@ public class TelaProduto extends Application implements EventHandler<ActionEvent
 		stage.setTitle("Gerenciamento de produto");
 		stage.show();
 	}
-
-	public void responsividadeLista() {
-		this.table.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Produto>() {
-			@Override
-			public void changed(ObservableValue<? extends Produto> arg0, Produto arg1, Produto arg2) {
-				if (arg2 != null) {
-				produtoParaView(arg2);					
-				}
+	
+	
+	
+	@Override
+	public void handle(ActionEvent e) {
+		if(e.getTarget() == btnPesquisar) {
+			pesquisarProduto();
+		}else if(e.getTarget() == btnSalvar) {
+			Produto p = viewParaProduto();
+			if(cp.produtoExiste(p)) {
+				cp.alterarProduto(p);
+			}else {
+				cp.inserirProduto(p);
 			}
-		});
+			limparCampos();
+		}else {
+			Produto p = viewParaProduto();
+			limparCampos();
+			cp.removerProduto(p);
+
+		}	
 	}
 
 	public void adicionarEventos() {
@@ -100,37 +138,67 @@ public class TelaProduto extends Application implements EventHandler<ActionEvent
 	}
 	
 	public void adicionandoFilhosPainel() {
+		
+		this.painelPrincipal.setTop(this.painelTopo);
+		this.painelPrincipal.setLeft(this.painelLateral);
+		this.painelPrincipal.setCenter(this.painelCentral);
+		
+		
+		this.painelCentral.getChildren().add(this.painelPesquisarElementos);
+		this.painelCentral.getChildren().add(linha);
+		this.painelCentral.getChildren().add(table);
+		
+		this.painelLateral.getChildren().add(this.painelLateral1);
+		this.painelLateral.getChildren().add(this.painelLateral2);
+		this.painelLateral.getChildren().add(this.painelLateral3);
+		this.painelLateral.getChildren().add(this.painelLateral4);
+		this.painelLateral.getChildren().add(this.painelLateral5);
+		this.painelLateral.getChildren().add(this.painelLateral6);
+		this.painelLateral.getChildren().add(this.painelLateral7);
+		this.painelLateral.getChildren().add(this.painelButtons);
+		
+		this.painelLateral1.getChildren().add(this.lblId);
+		this.painelLateral1.getChildren().add(this.tfId);
+		
+		this.painelLateral2.getChildren().add(this.lblNome);
+		this.painelLateral2.getChildren().add(this.tfNome);
+		
+		this.painelLateral3.getChildren().add(this.lblDescricao);
+		this.painelLateral3.getChildren().add(this.tfDescricao);
+		
+		this.painelLateral4.getChildren().add(this.lblMax);
+		this.painelLateral4.getChildren().add(this.tfMax);
+		
+		this.painelLateral5.getChildren().add(this.lblMin);
+		this.painelLateral5.getChildren().add(this.tfMin);
+		
+		this.painelLateral6.getChildren().add(this.lblTempoVida);
+		this.painelLateral6.getChildren().add(this.tfTempoVida);
+		
+		this.painelLateral7.getChildren().add(this.lblPreco);
+		this.painelLateral7.getChildren().add(this.tfPreco);
+		
+		this.painelButtons.getChildren().add(btnExcluir);	
+		this.painelButtons.getChildren().add(btnSalvar);
+		
+		this.painelPesquisarElementos.getChildren().add(this.tfPesquisar);
+		this.painelPesquisarElementos.getChildren().add(this.btnPesquisar);
+		
+		this.painelTopo.getChildren().add(img);
 
-		
-		painelPrincipal.setTop(img);
-		painelPrincipal.setLeft(painelLbl);
-		painelPrincipal.setCenter(painelManipulavel);
-		painelPrincipal.setBottom(painelBtn);
-		painelPrincipal.setRight(table);
-		
-		
-		painelLbl.add(lblId, 0, 0);
-		painelLbl.add(lblNome, 0, 1);
-		painelLbl.add(lblDescricao, 0, 2);
-		painelLbl.add(painelLbl2, 0, 5);
-		
-		painelLbl2.add(lblMax, 0, 0);
-		painelLbl2.add(lblMin, 0, 1);
-		painelLbl2.add(lblTempoVida, 0, 2);
-		painelLbl2.add(lblPreco, 0, 3);
-		
-		
-		painelManipulavel.add(tfId, 0, 0);
-		painelManipulavel.add(tfNome, 0, 1);
-		painelManipulavel.add(tfDescricao, 0, 2);
-		painelManipulavel.add(tfMax, 0, 3);
-		painelManipulavel.add(tfMin, 0, 4);
-		painelManipulavel.add(tfTempoVida, 0, 5);
-		painelManipulavel.add(tfPreco, 0, 6);
-		
-		painelBtn.getChildren().add(btnPesquisar);
-		painelBtn.getChildren().add(btnSalvar);
-		painelBtn.getChildren().add(btnExcluir);
+	}
+	
+	public void pesquisarProduto() {
+		Produto p = new Produto();
+		try {
+			int id = Integer.parseInt(tfPesquisar.getText());
+			p.setId(id);
+			p = cp.pesquisarProdutoNomeId(p);
+		} catch (NumberFormatException e) {
+			p.setNome(tfPesquisar.getText());
+			p = cp.pesquisarProdutoNome(p);
+		}
+		produtoParaView(p);
 	}
 
 	public void adicionandoEstiloElementos() {
@@ -143,6 +211,7 @@ public class TelaProduto extends Application implements EventHandler<ActionEvent
 		lblMin.setFont(new Font(20));
 		lblTempoVida.setFont(new Font(20));
 		lblPreco.setFont(new Font(20));
+
 		
 		lblId.setStyle("-fx-font-weight: bold;-fx-text-fill: #000000;");
 		lblNome.setStyle("-fx-font-weight: bold;-fx-text-fill: #000000;");
@@ -151,46 +220,70 @@ public class TelaProduto extends Application implements EventHandler<ActionEvent
 		lblMin.setStyle("-fx-font-weight: bold;-fx-text-fill: #000000;");
 		lblTempoVida.setStyle("-fx-font-weight: bold;-fx-text-fill: #000000;");
 		lblPreco.setStyle("-fx-font-weight: bold;-fx-text-fill: #000000;");
+
 		
-		btnPesquisar.setFont(new Font(20));
-		btnSalvar.setFont(new Font(20));
-		btnExcluir.setFont(new Font(20));
+		btnPesquisar.setFont(new Font(15));
+		btnSalvar.setFont(new Font(15));
+		btnExcluir.setFont(new Font(15));
+		
+		this.tfId.setPrefSize(200, 30);
+		this.tfNome.setPrefSize(200, 30);
+		this.tfDescricao.setPrefSize(200, 100);
+		this.tfMax.setPrefSize(200, 30);
+		this.tfMin.setPrefSize(200, 30);
+		this.tfTempoVida.setPrefSize(200, 30);
+		this.tfPreco.setPrefSize(200, 30);
+		this.tfPesquisar.setPrefSize(200, 32);
+		
+		//this.linha.setFill(Paint.valueOf(STYLESHEET_MODENA));
+		this.linha.setStroke(Color.BLACK);
+        this.linha.setStrokeWidth(5.0f);
+        
+		this.btnExcluir.setPrefWidth(145);
+		this.btnSalvar.setPrefWidth(200);
+		this.btnPesquisar.setPrefHeight(10);
+		
+		this.linha.setStartX(0);
+		this.linha.setEndX(530);
 	}
 
 	public void adicionandoMarginsPainel() {
-		Insets marginTop = new Insets(20, 20, 20, 550);
+
 		Insets margin = new Insets(20, 20, 20, 20);
-		Insets marginBot = new Insets(0, 0, 150,200);
-		Insets marginRight = new Insets(0, 50, 0, 100);
-		Insets marginTiraDesc = new Insets(160, 0, 0, 0);
-	
+		Insets marginRight = new Insets(0, 50, 0, 0);
+		Insets marginLeft = new Insets(0, 0, 0, 50);
+		Insets marginBottom = new Insets(0, 0, 20, 0);
+		Insets marginTable = new Insets(40, 40, 40, 40);
 		
-		this.painelPrincipal.setMargin(painelBtn, marginBot);
-		this.painelPrincipal.setMargin(table, marginRight);
+        BorderPane.setMargin(this.painelLateral, margin);
 
 		
-		this.painelLbl.setMargin(lblId, margin);
-		this.painelLbl.setMargin(lblNome, margin);
-		this.painelLbl.setMargin(lblDescricao, margin);
-		this.painelLbl.setMargin(painelLbl2, marginTiraDesc);
+		this.painelLateral1.setHgap(140);
+		this.painelLateral2.setHgap(104);
+		this.painelLateral3.setHgap(71);
+		this.painelLateral4.setHgap(45);
+		this.painelLateral5.setHgap(49);
+		this.painelLateral6.setHgap(20);
+		this.painelLateral7.setHgap(109);
+		this.painelButtons.setHgap(19);
+		this.painelPesquisarElementos.setHgap(5);
 		
+		
+		this.painelLateral.setMargin(this.painelLateral1, marginBottom);
+		this.painelLateral.setMargin(this.painelLateral2, marginBottom);
+		this.painelLateral.setMargin(this.painelLateral3, marginBottom);
+		this.painelLateral.setMargin(this.painelLateral4, marginBottom);
+		this.painelLateral.setMargin(this.painelLateral5, marginBottom);
+		this.painelLateral.setMargin(this.painelLateral6, marginBottom);
+		this.painelLateral.setMargin(this.painelLateral7, marginBottom);
 
-		this.painelLbl2.setMargin(lblMax, margin);
-		this.painelLbl2.setMargin(lblMin, margin);
-		this.painelLbl2.setMargin(lblTempoVida, margin);
-		this.painelLbl2.setMargin(lblPreco, margin);
+		this.painelCentral.setMargin(this.painelPesquisarElementos, marginBottom);
+		this.painelCentral.setMargin(this.linha, marginBottom);
 		
-		this.painelManipulavel.setMargin(tfId, margin);
-		this.painelManipulavel.setMargin(tfNome, margin);
-		this.painelManipulavel.setMargin(tfDescricao, margin);
-		this.painelManipulavel.setMargin(tfMax, margin);
-		this.painelManipulavel.setMargin(tfMin, margin);
-		this.painelManipulavel.setMargin(tfTempoVida, margin);
-		this.painelManipulavel.setMargin(tfPreco, margin);
+		this.painelPrincipal.setMargin(this.painelCentral, margin);
 		
-		this.painelBtn.setMargin(btnPesquisar, margin);
-		this.painelBtn.setMargin(btnSalvar, margin);
-		this.painelBtn.setMargin(btnExcluir, margin);
+		this.painelCentral.setMargin(this.table,  marginBottom);
+
 	}
 
 
@@ -255,66 +348,49 @@ public class TelaProduto extends Application implements EventHandler<ActionEvent
 		tfPreco.setText("");
 	}
 	
-	public static void main(String[] args) {
-		Application.launch(args);
-	}
-	
-	
-	@Override
-	public void handle(ActionEvent e) {
-		if(e.getTarget() == btnPesquisar) {
-			Produto p = viewParaProduto();
-			Produto p2 = new Produto();
-			p2 = cp.pesquisarProdutoNome(p);
-			produtoParaView(p2);
-		}else if(e.getTarget() == btnSalvar) {
-			Produto p = viewParaProduto();
-			if(cp.produtoExiste(p)) {
-				cp.alterarProduto(p);
-			}else {
-				cp.inserirProduto(p);
+
+
+	public void responsividadeLista() {
+		this.table.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Produto>() {
+			@Override
+			public void changed(ObservableValue<? extends Produto> arg0, Produto arg1, Produto arg2) {
+				if (arg2 != null) {
+				produtoParaView(arg2);					
+				}
 			}
-			limparCampos();
-		}else {
-			Produto p = viewParaProduto();
-			limparCampos();
-			cp.removerProduto(p);
-
-		}	
+		});
 	}
-	
-
 	
 	public void definirColunas() {
 
 		TableColumn<Produto, Number> colunaId = new TableColumn<>("Id");
 		colunaId.setCellValueFactory(itemData -> new ReadOnlyLongWrapper(itemData.getValue().getId()));
-		colunaId.setPrefWidth(80);
+		colunaId.setPrefWidth(40);
 
 		TableColumn<Produto, String> colunaNome = new TableColumn<>("Nome");
 		colunaNome.setCellValueFactory(itemData -> new ReadOnlyStringWrapper(itemData.getValue().getNome()));
-		colunaNome.setPrefWidth(100);
+		colunaNome.setPrefWidth(55);
 		
 		TableColumn<Produto, String> colunaDescricao = new TableColumn<>("Descricao");
 		colunaDescricao.setCellValueFactory(itemData -> new ReadOnlyStringWrapper(itemData.getValue().getDescricao()));
-		colunaDescricao.setPrefWidth(170);
+		colunaDescricao.setPrefWidth(130);
 
-		TableColumn<Produto, Number> qtdMaxColuna = new TableColumn<>("Qtd maxima");
+		TableColumn<Produto, Number> qtdMaxColuna = new TableColumn<>("Qtd Max");
 		qtdMaxColuna.setCellValueFactory(itemData -> new ReadOnlyIntegerWrapper(itemData.getValue().getQtdMax()));
-		qtdMaxColuna.setPrefWidth(130);
+		qtdMaxColuna.setPrefWidth(65);
 		
-		TableColumn<Produto, Number> qtdMinColuna = new TableColumn<>("Qtd minima");
+		TableColumn<Produto, Number> qtdMinColuna = new TableColumn<>("Qtd Min");
 		qtdMinColuna.setCellValueFactory(itemData -> new ReadOnlyIntegerWrapper(itemData.getValue().getQtdMin()));
-		qtdMinColuna.setPrefWidth(130);
+		qtdMinColuna.setPrefWidth(65);
 		
 		TableColumn<Produto, Number> tempoDeVidaColuna = new TableColumn<>("Tempo de vida");
 		tempoDeVidaColuna.setCellValueFactory(itemData -> new ReadOnlyIntegerWrapper(itemData.getValue().getQtdTempoVida()));
-		tempoDeVidaColuna.setPrefWidth(130);
+		tempoDeVidaColuna.setPrefWidth(100);
 		
 		
 		TableColumn<Produto, Number> colunaPreco = new TableColumn<>("Preco");
 		colunaPreco.setCellValueFactory(itemData -> new ReadOnlyDoubleWrapper(itemData.getValue().getPreco()));
-		colunaPreco.setPrefWidth(100);
+		colunaPreco.setPrefWidth(80);
 		
 		
 
