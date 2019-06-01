7e84f4d5034c9dd3a43b7d510d2d0df05aabb96a
bruno <bruno@192.168.0.12>
2019-06-01 18:26:28 -0300

    controleFuncionario feito

--- src/Control/ControleDeFuncionario.java
+++ src/Control/ControleDeFuncionario.java
@@ -1,5 +1,7 @@
 package Control;
 
+import javax.swing.JOptionPane;
+
 import Model.Funcionario;
 import Model.Produto;
 import javafx.collections.FXCollections;
@@ -10,15 +12,67 @@ public class ControleDeFuncionario {
 	
 	private ObservableList<Funcionario> listaFunc = FXCollections.observableArrayList();
 	
-	public void inserirFuncionario() {
-		
+	public void inserirFuncionario(Funcionario f) {
+		this.listaFunc.add(f);
+		JOptionPane.showMessageDialog(null,  "Funcionario adicionado com sucesso");
+	}
+	public Funcionario pesquisarFuncionarioNome(Funcionario func) {
+		Funcionario retornado = new Funcionario();
+		for(Funcionario f : listaFunc) {
+			if(f.getNome().equals(func.getNome())) {
+				JOptionPane.showMessageDialog(null,  "Funcionario Encontrado");
+				return f;
+			}
+		}
+		if(retornado.getId() == 0) {
+			JOptionPane.showMessageDialog(null,"Funcionario NÃO ENCONTRADO");
+		}
+		return retornado;
 	}
 	
-	public void alterarFuncionario() {
-		
+	public void alterarFuncionario(Funcionario func) {
+		for(Funcionario f : listaFunc) {
+			if(f.getNome().equals(func.getNome())) {
+				listaFunc.remove(f);
+				listaFunc.add(func);
+				JOptionPane.showMessageDialog(null,  "Funcionario alterado com sucesso");
+			}
+		}
 	}
 	
-	public void excluirFuncionario() {
-		
+	public void removerFuncionario(Funcionario func) {
+		for(Funcionario f : listaFunc) {
+			if(f.getNome().equals(func.getNome())) {
+				listaFunc.remove(f);
+				JOptionPane.showMessageDialog(null,  "Funcionario removido com sucesso");
+				break;
+			}
+		}
+	}
+	
+
+	
+	public boolean funcionarioExiste(Funcionario func) {
+		boolean existe = false;
+		for(Funcionario f : listaFunc) {
+			if(f.getNome().equals(func.getNome())) {
+				existe = true;
+			}
+		}
+		return existe;
+	}
+
+	public Funcionario pesquisarFuncionarioId(Funcionario func) {
+		Funcionario retornado = new Funcionario();
+		for(Funcionario f : listaFunc) {
+			if(f.getId() == func.getId()) {
+				JOptionPane.showMessageDialog(null,  "Funcionario Encontrado");
+				return f;
+			}
+		}
+		if(retornado.getId() == 0) {
+			JOptionPane.showMessageDialog(null,"FUNCIONARIO NÃO ENCONTRADO");
+		}
+		return retornado;
 	}
 }
